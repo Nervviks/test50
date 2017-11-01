873697f201c9bff483f7e9f53fef201e81fb4f46
Nervviks <Nervvikss@gmail.com>
2017-11-01 22:01:13 +0200

    vhj

---
+++ Roze/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+//	int k,l=10,c=0;
+	int mas[]=new int[10];
+//	k=mas[0];
+//	for(int k1=0;k1<10;k1++) {
+//		mas[k1]=k1;
+//	}
+//	for(int i=0;i < l;i++) {
+//		if(i!=l-1) {
+//		mas[i]=mas[i+1];
+//		}else {
+//			mas[l-1]=0;
+//		}
+//	}
+//	for (int i : mas) {
+//		System.out.println(i);
+//	}
+		ArrayList<Integer> stack=new ArrayList<>();
+		stack.add(2);
+		stack.add(5);
+		stack.remove(0);
+		System.out.println(mas[0]);
+	}
+
+}
